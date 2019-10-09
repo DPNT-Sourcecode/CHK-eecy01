de5d765c666940d5d2380c8db553881ea582b9f9
sbx_user1051 <sbx_user1051@169.254.185.37>
2019-10-09 16:20:50 +0000

    Wed Oct 09 16:20:50 UTC 2019

--- test/solution_tests/test_chk_r1.py
+++ test/solution_tests/test_chk_r1.py
@@ -1,6 +1,13 @@
 from solutions.CHK import checkout_solution
 
 class TestCheckout():
+
+    def test_skus_none(self):
+        assert checkout_solution.checkout(None) == -1
+        
+    def test_skus_empty(self):
+        assert checkout_solution.checkout('') == -1
+        
     def test_skus_number(self):
         assert checkout_solution.checkout('AB12CD') == -1
         
@@ -11,4 +18,7 @@ class TestCheckout():
         assert checkout_solution.checkout('A;B') == -1
 
     def test_skus(self):
-        assert checkout_solution.checkout('AB') == 80 
+        assert checkout_solution.checkout('AB') == 80
+
+    def test_skus_lowcase(self):
+        assert checkout_solution.checkout('ABab') == 80
