774e9949fb868db6876f87258e80114565abc6e0
sbx_user1051 <sbx_user1051@169.254.199.21>
2019-10-09 19:19:43 +0000

    Wed Oct 09 19:19:43 UTC 2019

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -9,12 +9,12 @@ SPECIAL_OFFER_KEYS = SPECIAL_OFFER.keys()
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
-    if not skus or not str.isalpha(skus):
+    if not str.isalpha(skus):
         return -1
     skus = skus.upper()
     
     skus_counter = Counter(skus)
-    skus_sum = sum(map(count_price, skus_counter.items()))
+    skus_sum = sum(map(count_price, skus_counter.items()), 0)
     return skus_sum
 
 def count_price(checkout_item):
@@ -32,3 +32,4 @@ def count_price(checkout_item):
         promotion_price = int(item_count / promo_count) * SPECIAL_OFFER[item][1]
     total_price = promotion_price + PRICES[item] * remainder
     return total_price
+
