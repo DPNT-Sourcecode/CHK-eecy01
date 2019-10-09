964e3614c4e2f9b30ed70ceef737a9cb97e7b6ae
sbx_user1051 <sbx_user1051@169.254.185.37>
2019-10-09 16:32:50 +0000

    Wed Oct 09 16:32:50 UTC 2019

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,7 +1,7 @@
 
 from collections import Counter
 
-PRICES = {'A': 50, 'B': 30, 'C':20, 'D':15}
+PRICES = {'A': 50, 'B': 30, 'C': 20, 'D': 15}
 
 # noinspection PyUnusedLocal
 # skus = unicode string
@@ -11,6 +11,8 @@ def checkout(skus):
     skus = skus.upper()
     
     skus_counter = Counter(skus)
+    skus_sum = sum(map(count_price, skus_counter))
     return skus_counter
 
 def count_price(item, item_count):
+    return PRICES[item] * item_count
