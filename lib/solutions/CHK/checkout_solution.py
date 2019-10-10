f76cdbbed4e166d2b11b14454a238bf5a99bb659
sbx_user1051 <sbx_user1051@169.254.31.101>
2019-10-10 21:17:19 +0000

    Thu Oct 10 21:17:19 UTC 2019

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -69,7 +69,8 @@ class CheckoutMachine():
             discount = self.discounts.xfree_for_y[discount_prod]
             for discount_quantity in discount.keys():
                 if basket[discount_prod] >= discount_quantity:
-                    actual_discounts.update(discount[discount_quantity])
+                    multiple = int(basket[discount_prod] / discount_quantity)
+                    actual_discounts.update(discount[discount_quantity]*multiple)
         actual_discounts = Counter(actual_discounts)
         return basket - actual_discounts
 
@@ -79,5 +80,6 @@ class CheckoutMachine():
         cm_basket = self.apply_free_discount(cm_basket)
         total_price = sum(map(self.count_price, cm_basket.items()), 0)
         return total_price
+
 
 
