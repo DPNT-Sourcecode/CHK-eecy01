460d498e0166f01f4e605df5167d7274ea9541ab
sbx_user1051 <sbx_user1051@169.254.31.101>
2019-10-10 18:35:44 +0000

    Thu Oct 10 18:35:44 UTC 2019

--- test/solution_tests/test_chk_r2.py
+++ test/solution_tests/test_chk_r2.py
@@ -6,13 +6,15 @@ from solutions.CHK import checkout_solution
 
 class TestCheckoutMachine():
 
-    def setUp(self):
+    @classmethod
+    def setup_class(cls):
         products = checkout_solution.Products()
         discounts = checkout_solution.DiscountStore()
-        self.cm = checkout_solution.CheckoutMachine(products, discounts)
+        cls.cm = checkout_solution.CheckoutMachine(products, discounts)
         
     def test_apply_free_discount_not(self):
         basket = Counter({'B': 1, 'E': 1})
         expected_basket = {'B': 1, 'E': 1}
         assert self.cm.apply_free_discount(basket) == expected_basket
+
 
