5396906ae69cb3824786c7bb2df080be212c2f3a
sbx_user1051 <sbx_user1051@169.254.31.101>
2019-10-10 20:23:44 +0000

    Thu Oct 10 20:23:44 UTC 2019

--- test/solution_tests/test_chk_r2.py
+++ test/solution_tests/test_chk_r2.py
@@ -15,36 +15,31 @@ class TestCheckoutMachine():
     def test_apply_free_discount_B1E1(self):
         basket = Counter({'B': 1, 'E': 1})
         expected_basket = {'B': 1, 'E': 1}
-        self.cm.apply_free_discount(basket)
-        assert basket == expected_basket
+        assert self.cm.apply_free_discount(basket) == expected_basket
 
     def test_apply_free_discount_B0E2(self):
         basket = Counter({'B': 0, 'E': 2})
-        expected_basket = {'B': 0, 'E': 2}
-        self.cm.apply_free_discount(basket)
-        assert basket == expected_basket   
+        expected_basket = {'E': 2}
+        assert self.cm.apply_free_discount(basket) == expected_basket
     
     def test_apply_free_discount_B1E2(self):
         basket = Counter({'B': 1, 'E': 2})
-        expected_basket = {'B': 0, 'E': 2}
-        self.cm.apply_free_discount(basket)
-        assert basket == expected_basket
+        expected_basket = {'E': 2}
+        assert self.cm.apply_free_discount(basket) == expected_basket
 
     def test_apply_free_discount_B2E1(self):
         basket = Counter({'B': 2, 'E': 1})
         expected_basket = {'B': 2, 'E': 1}
-        self.cm.apply_free_discount(basket)
-        assert basket == expected_basket        
+        assert self.cm.apply_free_discount(basket) == expected_basket
 
     def test_apply_free_discount_B2E2(self):
         basket = Counter({'B': 2, 'E': 2})
         expected_basket = {'B': 1, 'E': 2}
-
         assert self.cm.apply_free_discount(basket) == expected_basket
 
     def test_apply_free_discount_E2(self):
         basket = Counter({'E': 2})
         expected_basket = {'E': 2}
-        self.cm.apply_free_discount(basket)
-        assert basket == expected_basket        
+        assert self.cm.apply_free_discount(basket) == expected_basket
+
 
