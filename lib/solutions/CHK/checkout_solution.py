1e84cbbc1f75b480b4c2312f31493204e6366d88
sbx_user1051 <sbx_user1051@169.254.114.221>
2019-10-14 17:20:20 +0000

    Mon Oct 14 17:20:20 UTC 2019

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -99,9 +99,3 @@ class CheckoutMachine():
         cm_basket = self.apply_free_discount(cm_basket)
         total_price = sum(map(self.count_price, cm_basket.items()), 0)
         return total_price
-
-
-
-
-
-
