35cc3eeda1be177223258304249f2435fa0df68c
sbx_user1051 <sbx_user1051@169.254.114.221>
2019-10-14 16:48:39 +0000

    Mon Oct 14 16:48:39 UTC 2019

--- test/solution_tests/test_chk_r3.py
+++ test/solution_tests/test_chk_r3.py
@@ -54,4 +54,40 @@ class TestCheckoutMachine():
     def test_apply_free_discount_F7(self):
         basket = Counter({'F': 7})
         expected_basket = {'F': 5}
-        assert self.cm.apply_free_discount(basket) == expected_basket         
+        assert self.cm.apply_free_discount(basket) == expected_basket
+        
+    def test_get_total_price_B1E2F3(self):
+        basket = Counter({'B': 1, 'E': 2, 'F': 3})
+        assert self.cm.get_total_price(basket) == 100
+
+    def test_get_total_price_B0E2F3(self):
+        basket = Counter({'B': 0, 'E': 2, 'F': 3})
+        assert self.cm.get_total_price(basket) == 100
+        
+    def test_get_total_price_B2E2F1(self):
+        basket = Counter({'B': 2, 'E': 2, 'F': 1})
+        assert self.cm.get_total_price(basket) == 120
+        
+    def test_get_total_price_B2E2F2(self):
+        basket = Counter({'B': 2, 'E': 2, 'F': 2})
+        assert self.cm.get_total_price(basket) == 130
+
+    def test_get_total_price_B2E2F3(self):
+        basket = Counter({'B': 2, 'E': 2, 'F': 3})
+        assert self.cm.get_total_price(basket) == 130
+
+    def test_get_total_price_F4(self):
+        basket = Counter({'F': 4})
+        assert self.cm.get_total_price(basket) == 30
+
+    def test_get_total_price_F5(self):
+        basket = Counter({'F': 5})
+        assert self.cm.get_total_price(basket) == 40
+
+    def test_get_total_price_F6(self):
+        basket = Counter({'F': 6})
+        assert self.cm.get_total_price(basket) == 40
+
+    def test_get_total_price_F7(self):
+        basket = Counter({'F': 7})
+        assert self.cm.get_total_price(basket) == 50
