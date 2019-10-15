5949d9a83944bb02d060fed21911122a2c4b0223
sbx_user1051 <sbx_user1051@169.254.83.93>
2019-10-15 15:49:47 +0000

    Tue Oct 15 15:49:47 UTC 2019

--- test/solution_tests/test_chk_r5.py
+++ test/solution_tests/test_chk_r5.py
@@ -18,6 +18,27 @@ class TestCheckoutMachine():
         assert new_basket == expected_basket
         assert total_discount_price == 0
         
+    def test_apply_group_discount_S1T1(self):
+        basket = Counter({'S': 1, 'T': 1})
+        expected_basket = {'S': 1, 'T': 1}
+        new_basket, total_discount_price = self.cm.apply_group_discount(basket)
+        assert new_basket == expected_basket
+        assert total_discount_price == 0
+
+    def test_apply_group_discount_S1T1X1(self):
+        basket = Counter({'S': 1, 'T': 1, 'X': 1})
+        expected_basket = {}
+        new_basket, total_discount_price = self.cm.apply_group_discount(basket)
+        assert new_basket == expected_basket
+        assert total_discount_price == 45
+
+    def test_apply_group_discount_S4T3Z2(self):
+        basket = Counter({'S': 4, 'T': 3, 'Z': 2})
+        expected_basket = {}
+        new_basket, total_discount_price = self.cm.apply_group_discount(basket)
+        assert new_basket == expected_basket
+        assert total_discount_price == 135   
+        
     def test_get_total_price_S1T1(self):
         basket = Counter({'S': 1, 'T': 1})
         assert self.cm.get_total_price(basket) == 40
